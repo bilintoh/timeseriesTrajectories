ec00cbf61e085045245adfbfec38578b3f2d1257
bilintoh <60271208+bilintoh@users.noreply.github.com>
2023-11-09 15:55:57 -0500

    Made the y-axis lable for the satckedbarPlot function dynamic.

--- R/stackbarPlot.R
+++ R/stackbarPlot.R
@@ -68,7 +68,7 @@ stackbarPlot <- function(input,
     guides(fill = guide_legend(title = "")) +
     geom_abline(slope=0, intercept=0,  col = "grey",size = 0.5) +
     xlab("Time Interval") +
-    ylab(paste("Annual",input[[7]],"Change (% of region)"))+
+    ylab = input[[10]]+
     ggtitle(input[[5]])+
     theme(
       plot.title = element_text(size = titleSize, face = "bold"))+
@@ -110,8 +110,8 @@ stackbarPlot <- function(input,
       panel.background = element_rect(fill = "transparent",colour = NA))+
     theme(axis.text.x=element_blank())+
     xlab("All time intervals") +
-    ylab(paste("Annual",input[[7]],"Change (% of region)"))+
-    ggtitle(input[[5]])+
+    ylab = input[[10]] +
+    ggtitle(input[[5]]) +
     theme(
       plot.title = element_text(size = titleSize, face = "bold"))+
     theme(
