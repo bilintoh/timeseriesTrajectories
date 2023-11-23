5828cf52ceb12db0d9a4a073b44754a406749900
bilintoh <60271208+bilintoh@users.noreply.github.com>
2023-11-22 20:49:48 -0500

    I updated the unified size, so that a list collect all the sizes for later aggregation.

--- R/rasterstackData.R
+++ R/rasterstackData.R
@@ -78,7 +78,7 @@ rasterstackData <- function(x,
     sumLastFirst_2[[i]] <- d3
 
     if(spatialextent == "unified" & zeroabsence == "yes" & annualchange == 'no'){
-      lengthSpext <- sum(unlist(newdf$max))
+      lengthSpext[[i]] <- sum(unlist(newdf$max))
       stackTitle <- paste("Change in presence of",categoryName,"category where extent is",regionName)
       yaxislable <- "Change (% of region)"
     } else if(spatialextent == 1 & zeroabsence == "yes" & annualchange == 'no'){
@@ -101,7 +101,7 @@ rasterstackData <- function(x,
       stackTitle <- paste("Change in presence of",categoryName,"category where extent is",regionName)
       yaxislable <- "Change (% of region)"
     }else if(spatialextent == "unified" & zeroabsence == "no" & annualchange == 'yes'){
-      lengthSpext <- sum(unlist(newdf$max))
+      lengthSpext[[i]] <- sum(unlist(newdf$max))
       stackTitle <- paste("Annual Change in presence of",categoryName,"category where extent is",regionName)
       yaxislable <- "Annual Change (% of region)"
     }else if(spatialextent == 1 & zeroabsence == "no" & annualchange == 'yes'){
@@ -113,7 +113,7 @@ rasterstackData <- function(x,
       stackTitle <- paste("Annual Change in presence of",categoryName,"category where extent is",regionName)
       yaxislable <- "Annual Change (% of region)"
     }else if(spatialextent == "unified" & zeroabsence == "yes" & annualchange == 'yes'){
-      lengthSpext <- sum(unlist(newdf$max))
+      lengthSpext[[i]] <- sum(unlist(newdf$max))
       stackTitle <- paste("Annual Change in presence of",categoryName,"category where extent is",regionName)
       yaxislable <- "Annual Change (% of region)"
     }
